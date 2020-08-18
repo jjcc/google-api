4f3b35735ebb1d6e185572a01aec830a2051a041
jjcc <jjccforth@gmail.com>
2020-08-18 14:24:48 -0400

    get all closed data today

--- Finance/ExTushare1.py
+++ Finance/ExTushare1.py
@@ -18,21 +18,30 @@ def get_indices():
         df.to_csv(f"data/indices/{m}_index.csv")
 
 
-def get_all_stock_info():
-    # df_close = ts.get_today_all()
+
+def get_all_stock_basics():
     df_basics = ts.get_stock_basics()
-    # df_all = pd.merge(left=df_close, right=df_basics, on='name', how='outer')
-    # df_all.to_csv(u"data/meta/all_stocks.csv")
     df_basics.to_csv(u"data/meta/all_stocks_basics.csv")
     print("done with get all")
 
+def get_all_stock_info():
+    df_close = ts.get_today_all()
+    #df_all = pd.merge(left=df_close, right=df_basics, on='name', how='outer')
+    #df_all.to_csv(u"data/meta/all_stocks.csv")
+    df_close.to_csv(u"data/meta/all_stocks_close.csv")
 
-# get_indices()
 
-get_all_stock_info()
 
 # df = pro.index_basic(market='SW')
 # df = pro.index_daily(ts_code='801210.SI', start_date='20180101',end_date='20181001' )
 # print(len(df.index))
 # df.to_cvs("801193SI.csv")
-print(ts.__version__)
+
+
+def main():
+    print(ts.__version__)
+    #get_indices()
+    get_all_stock_info()
+
+if __name__ == "__main__":
+    main()
