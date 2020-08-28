651a7e56f57c4e606fdf3cc5d631c5313e663775
jjcc <jjccforth@gmail.com>
2020-08-28 15:56:12 -0400

    fixed encoding problem

--- Finance/helper.py
+++ Finance/helper.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import chevron
 
 
@@ -10,9 +12,6 @@ str_post = '''    </div>
 '''
 
 
-import chevron
-
-
 dict_industry = { "KBE":"Bank","KRE":"Regional Banking","KCE":"Capital Markets","KIE":"Insurance",
                   "XAR":"Aerospace & Defense","XTN":"Transportation","XBI":"Biotech","XPH":"Pharmaceuticals",
                   "XHE":"Health Care Equipment","XHS":"Health Care Services","XOP":"Oil & Gas Exploration & Production",
@@ -21,14 +20,14 @@ dict_industry = { "KBE":"Bank","KRE":"Regional Banking","KCE":"Capital Markets",
                   "XITK":"FactSet Innovative Technology","XTL":"Telecom","XWEB":"Internet"
                 }
 
-dict_smartbeta = {"SPYD":"SPDR Portfolio S&P 500 High Dividend","SDY":"SPDR S&P Dividend","WDIV":"SPDR S&P Global Dividend",
-                "DWX":"SPDR S&P International Dividend","EDIV":"SPDR S&P Emerging Markets Dividend",
-                "QUS":"SPDR MSCI USA StrategicFactors","QWLD":"SPDR MSCI World StrategicFactors",
-                "QEFA":"SPDR MSCI EAFE StrategicFactors","QEMM":"SPDR MSCI Emerging Markets StrategicFactors",
-                "ONEY":"SPDR Russell 1000 Yield Focus","ONEV":"SPDR Russell 1000 Low Volatility Focus",
-                "ONEO":"SPDR Russell 1000 Momentum Focus","LGLV":"SPDR SSGA US Large Cap Low Volatility Index",
-                "SMLV":"SPDR SSGA US Small Cap Low Volatility Index","MMTM":"SPDR S&P 1500 Momentum Tilt",
-                "VLU":"SPDR S&P 1500 Value Tilt","DWFI":"SPDR Dorsey Wright® Fixed Income Allocation"
+dict_smartbeta = {"SPYD":"Portfolio S&P 500 High Dividend","SDY":"S&P Dividend","WDIV":"S&P Global Dividend",
+                "DWX":"S&P International Dividend","EDIV":"S&P Emerging Markets Dividend",
+                "QUS":"MSCI USA StrategicFactors","QWLD":"MSCI World StrategicFactors",
+                "QEFA":"MSCI EAFE StrategicFactors","QEMM":"MSCI Emerging Markets StrategicFactors",
+                "ONEY":"Russell 1000 Yield Focus","ONEV":"Russell 1000 Low Volatility Focus",
+                "ONEO":"Russell 1000 Momentum Focus","LGLV":"SSGA US Large Cap Low Volatility Index",
+                "SMLV":"SSGA US Small Cap Low Volatility Index","MMTM":"S&P 1500 Momentum Tilt",
+                "VLU":"S&P 1500 Value Tilt","DWFI":"Dorsey Wright® Fixed Income Allocation"
                   }
 
 
@@ -66,14 +65,14 @@ def render_sections(template_file, output_file,dict_input=dict_industry):
         str_seg += str_post
         str_all += str_seg
 
-    with open(output_file, 'w') as fout:
+    with open(output_file, 'w', encoding='utf-8') as fout:
         fout.write(str_all)
 
 
 def test_render():
     templt = 'single_item.templt'
-    file_res = 'generated.html'
-    render_sections(templt,file_res)
+    file_res = 'generated_sb.html'
+    render_sections(templt,file_res,dict_smartbeta)
 
 def main():
     test_render()
