62d3d1dbc3471d6e5445a836d914a473004bf215
jjcc <jjccforth@gmail.com>
2020-12-13 01:32:56 -0500

    more refactor

--- Finance/ts_helper.py
+++ Finance/ts_helper.py
@@ -36,13 +36,14 @@ def get_latest():
     latest_str = row[0]
     print(latest_str)
 
-def get_a_stock_info(stock,start, end ,ts, connection=None , intodb=True):
+def get_a_stock_info(stock,start, end ,ts, connection=None , tablename="stock2", intodb=True):
     '''
-
-    :param stock:
+    get/load one stock info (into DB)
+    :param stock: the stock code
     :param start: start date , as format of 'yyyymmdd'
     :param end: end date , as format of 'yyyymmdd'
-    :param connection:
+    :param ts: tushare handler
+    :param connection: db connection
     :return:
     '''
     #df = pro.index_daily(ts_code='600506.SI', start_date='20200101')
@@ -67,18 +68,19 @@ def get_a_stock_info(stock,start, end ,ts, connection=None , intodb=True):
         conn = connection
 
     if intodb:
-        df.to_sql(name='stocks2', con=conn, index=False, if_exists='append')
+        df.to_sql(name=tablename, con=conn, index=False, if_exists='append')
     print("done get a stock info")
 
 
 def get_list_of_stock_info(list, start, end,ts, conn = None):
     '''
+    Lodd list of stock info into DB
 
-    :param list:
-    :param start:
-    :param end:
-    :param ts:
-    :param conn:
+    :param list: the list of code needs to load
+    :param start: start date
+    :param end: end date
+    :param ts: tushare handler
+    :param conn: DB connection
     :return:
     '''
     for stk in list:
@@ -86,3 +88,28 @@ def get_list_of_stock_info(list, start, end,ts, conn = None):
         stkp = fadd_postfix(stk)
         print(f"retriving {stk} as {stkp}")
         get_a_stock_info(stkp, start,end, ts,conn)
+
+def load_lastday_to_db(file,targetlist,trade_date, dbname='sw_index2.db',tablename='stock2'):
+    '''
+    load stock info of the last day into DB
+    :param file: the csv file of one day all stock info harvested from other scripts
+    :param targetlist: the code list of selected stocks
+    :return:
+    '''
+    df_raw = pd.read_csv(file)
+    #trade=:close, settlement:pre_close, changepercent:pct_chg, volume:vol
+    df = df_raw[['code','open','high','low','trade','settlement','changepercent','volume','amount']]
+    df = df.rename(columns={'trade':'close','settlement':'pre_close','changepercent':'pct_chg','volume':'vol','code':'ts_code'})
+    df['pct_chg'] = df['pct_chg'].apply(lambda x: round(x, 3))
+    df['vol'] = df['vol'].apply(lambda x: x/100)
+    df['amount'] = df['amount'].apply(lambda x:x/1000)
+    df['change'] = df['close']- df['pre_close']
+    df['change'] = df['change'].apply(lambda x: round(x, 2))
+    df['trade_date'] = trade_date
+    df = df[df['ts_code'].isin(targetlist)]
+    df['ts_code'] = df['ts_code'].apply(lambda x: str(x).zfill(6))
+    df['ts_code'] = df['ts_code'].apply(fadd_postfix)
+    df = df.drop_duplicates()
+    #df.to_csv(u'data/temp/test_renamed.csv')
+    conn = sqlite3.connect(dbname)
+    df.to_sql(name=tablename, con=conn, index=False, if_exists='append')
